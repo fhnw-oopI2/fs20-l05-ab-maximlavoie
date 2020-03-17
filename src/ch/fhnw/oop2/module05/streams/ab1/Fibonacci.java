2622196c6a4d3e60e859e35ab551b64518903b0f
Maxim <maxim.lavoie@students.fhnw.ch (s)>
2020-03-17 11:44:14 +0100

    Fibonacci gelöst

---
+++ src/ch/fhnw/oop2/module05/streams/ab1/Fibonacci.java
@@ -0,0 +1,13 @@
+package ch.fhnw.oop2.module05.streams.ab1;
+
+import java.util.stream.Stream;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Stream.iterate(new Tuple<Integer>(1, 1), (n) -> (new Tuple(n.t2, n.t1 + n.t2))).limit(30)
+				.forEach(n -> System.out.println(n.t1));
+
+	}
+
+}
